f7095493c14b6ea92e14c1ad81cc67d05542764f
bast-i <m.bastug@me.com>
2022-10-31 13:30:13 +0100

    Kunde1

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -8,13 +8,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @ConfigProperty(name = "kundenname")
+    String kundenname;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Laptop";
+        return "Webpage für " + kundenname;
     }
 
 
